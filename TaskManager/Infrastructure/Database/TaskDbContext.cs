89ac9612a4da887cd722f4545b2fa1a885f121fe
Pedro <pedromendes7182@gmail.com>
2025-10-31 17:42:04 -0300

    feat(Context): adiciona o contexto da aplicação

---
+++ TaskManager/Infrastructure/Database/TaskDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Infrastructure.Database;
+
+public class TaskDbContext(DbContextOptions<TaskDbContext> options) : DbContext(options)
+{
+    public DbSet<Task> Tasks { get; set; }
+}
