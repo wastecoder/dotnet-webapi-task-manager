5bd1d5d42c7574279086b0f706cb1c4e882ed3f9
Pedro <pedromendes7182@gmail.com>
2025-10-31 17:27:21 -0300

    feat(Task): cria a entidade e o enum

---
+++ TaskManager/Domain/Entities/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Domain.Entities;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime DueDate { get; set; }
+    public TaskStatus Status { get; set; }
+}
---
+++ TaskManager/Domain/Enums/TaskStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Domain.Enums;
+
+public enum TaskStatus
+{
+    Pending = 0,
+    Running = 1,
+    Completed = 2,
+    Cancelled = 3,
+}
