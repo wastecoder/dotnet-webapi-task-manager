33ddeccfc642087edbb44b7bd13afb780b0f91a0
Pedro <pedromendes7182@gmail.com>
2025-11-03 05:30:24 -0300

    feat(TaskController): adiciona o endpoint GetAll

--- TaskManager/Controllers/TaskController.cs
+++ TaskManager/Controllers/TaskController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TaskManager.Domain.DTOs.Task;
 using TaskManager.Infrastructure.Database;
 using TaskManager.Mappers;
@@ -24,5 +25,23 @@ public class TaskController : ControllerBase
         _context.SaveChanges();
 
         return CreatedAtAction(nameof(Post), new { id = newTask.Id }, newTask.ToResponseDto());
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TaskResponseDto>>> GetAll(
+        [FromQuery] int page = 1, 
+        [FromQuery] int pageSize = 5)
+    {
+        if (page <= 0) page = 1;
+        if (pageSize <= 0) pageSize = 5;
+
+        var tasks = await _context.Tasks
+            .OrderBy(t => t.Id)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        var response = tasks.Select(t => t.ToResponseDto());
+        return Ok(response);
     }
 }
